6949fe7ad7db302d94e5b6facd8535c82aa807e0
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-28 11:03:56 -0700

    add submit handler for PhotoFormView

--- app/assets/javascripts/views/photo_form_view.js
+++ app/assets/javascripts/views/photo_form_view.js
@@ -3,6 +3,7 @@
 
   var PhotoFormView = PT.PhotoFormView = function () {
     this.$el = $("<div></div>");
+    this.$el.on("submit", "form", this.submit.bind(this));
   };
 
   _.extend(PhotoFormView.prototype, {
@@ -12,6 +13,17 @@
       this.$el.html(JST["photo_form"]());
 
       return this;
+    },
+
+    submit: function (event) {
+      event.preventDefault();
+
+      var data = $(event.currentTarget).serializeJSON();
+
+      var photo = new PT.Photo(data.photo);
+      photo.create(function (photo) {
+        console.log("Photo got saved!");
+      });
     }
   });
 })(this);
