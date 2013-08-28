fb7d8d8331bf4dffd5929c01b11c2bafa0cea9eb
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-28 12:51:05 -0700

    write routing helping methods

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,13 +22,27 @@
 //
 //= require_tree .
 
-PT.initialize = function () {
-  var content = $("#content");
-  PT.Photo.fetchByUserId(CURRENT_USER_ID, function () {
+_.extend(PT, {
+  initialize:   function () {
+    PT.Photo.fetchByUserId(CURRENT_USER_ID, function () {
+      PT.showPhotosIndex();
+    });
+  },
+
+  showPhotoDetail: function (photo) {
+    var content = $("#content");
+
+    var photoDetailView = new PT.PhotoDetailView(photo);
+    content.html(photoDetailView.render().$el);
+  },
+
+  showPhotosIndex: function () {
+    var content = $("#content");
+
     var photosListView = new PT.PhotosListView();
     content.append(photosListView.render().$el);
 
     var photoFormView = new PT.PhotoFormView();
     content.append(photoFormView.render().$el);
-  });
-};
+  },
+});
