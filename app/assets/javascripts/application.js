0918c2b3a3347c114980408aeb6ebf9c69c9511c
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-28 00:28:18 -0700

    begin writing Photo JS model

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,57 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
+// This is a manifest file that'll be compiled into application.js,
+// which will include all the files listed below.
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+// Any JavaScript/Coffee file within this directory,
+// lib/assets/javascripts, vendor/assets/javascripts, or
+// vendor/assets/javascripts of plugins, if any, can be referenced
+// here using a relative path.
 //
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
+// It's not advisable to add code directly here, but if you do, it'll
+// appear at the bottom of the the compiled file.
 //
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE
+// PROCESSED, ANY BLANK LINE SHOULD GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
 //= require jquery_ujs
+//= require underscore
 //= require_tree .
+
+function Photo (data) {
+  this.attributes = _.extend({}, data);
+}
+
+_.extend(Photo, {
+  fetchByUserId: function (userId, callback) {
+    $.ajax({
+      url: "/users/" + userId + "/photos",
+      type: "GET",
+      success: function (photosData) {
+        var photos = _(photosData).map(function (photoData) {
+          return new Photo(photoData);
+        });
+
+        callback(photos);
+      }
+    });
+  }
+});
+
+_.extend(Photo.prototype, {
+  get: function (attrName) {
+    this.attributes[attrName];
+  },
+
+  set: function (attrName, val) {
+    this.attributes[attrName] = val
+  },
+
+  create: function (callback) {
+    $.ajax({
+      url: "/photos",
+      type: "POST",
+      data: this.attributes,
+      success: callback
+    });
+  }
+});
