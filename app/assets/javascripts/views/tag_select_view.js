2deabb6ba975aa06a2cf860e10fb73f52871227a
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-28 18:32:38 -0700

    add TagOptionsView#selectTagOption method

--- app/assets/javascripts/views/tag_select_view.js
+++ app/assets/javascripts/views/tag_select_view.js
@@ -6,11 +6,22 @@
     this.photo = photo;
 
     var imgPos = $(event.currentTarget).position();
+    this.tagPos = {
+      x_pos: event.offsetX - 50,
+      y_pos: event.offsetY - 50
+    };
+
     this.$el.css({
       position: "absolute",
-      left: event.offsetX + imgPos.left - 50,
-      top: event.offsetY + imgPos.top - 50
+      left: this.tagPos.x_pos + imgPos.left,
+      top: this.tagPos.y_pos + imgPos.top
     });
+
+    this.$el.on(
+      "click",
+      "ul.tag-options li",
+      this.selectTagOption.bind(this)
+    );
   };
 
   _.extend(TagSelectView.prototype, {
@@ -26,6 +37,18 @@
       }));
 
       return this;
+    },
+
+    selectTagOption: function (event) {
+      var userId = $(event.currentTarget).attr("data-id");
+      new PT.PhotoTagging({
+        user_id: userId,
+        photo_id: this.photo.get("id"),
+        x_pos: this.tagPos.x_pos,
+        y_pos: this.tagPos.y_pos
+      }).create();
+
+      this.$el.remove();
     }
   });
 })(this);
