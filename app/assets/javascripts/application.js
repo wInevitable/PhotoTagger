08824577d0516facb91089a4644c9e41607cc5f0
CJ Avilla <cjavilla@gmail.com>
2014-01-07 10:39:32 -0800

    remove solution from skeleton

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,29 +21,3 @@
 //= require_tree ../templates
 //
 //= require_tree .
-
-_.extend(PT, {
-  initialize:   function () {
-    PT.Photo.fetchByUserId(CURRENT_USER_ID, function () {
-      PT.showPhotosIndex();
-    });
-  },
-
-  showPhotoDetail: function (photo) {
-    var content = $("#content");
-
-    var photoDetailView = new PT.PhotoDetailView(photo);
-    content.html(photoDetailView.render().$el);
-  },
-
-  showPhotosIndex: function () {
-    var content = $("#content");
-    content.empty();
-
-    var photosListView = new PT.PhotosListView();
-    content.append(photosListView.render().$el);
-
-    var photoFormView = new PT.PhotoFormView();
-    content.append(photoFormView.render().$el);
-  },
-});
